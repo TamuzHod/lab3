9505df1764da09334adcddb3cf5857823569ef8a
Tamuz <Tamuzhod@gmail.com>
2019-01-24 13:48:38 -0800

    all good

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is connected? \n Yes it is");
+		$('.jumbotron p').toggleClass("active");
+		$("#testjs").text("Please wait ...");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) {
+	//prevent this page from loading
+	e.preventDefault();
+
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the Project.</p></div>");
+	} else {
+		$(".project-description").fadeToggle();
+	}
+
+
+}
+
